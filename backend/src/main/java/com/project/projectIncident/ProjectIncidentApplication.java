33b511caa8340ca8fa99f3f079c0a06c8e35e999
auriceliof <auricelio.suporte@hotmail.com>
2023-06-14 23:22:22 -0300

    Initial

---
+++ backend/src/main/java/com/project/projectIncident/ProjectIncidentApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectIncident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIncidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIncidentApplication.class, args);
+	}
+
+}
