b9f59564e7f8dbd9505a5af957835c2be6f5fa9f
auriceliof <auricelio.suporte@hotmail.com>
2023-06-15 23:02:01 -0300

    ADD: SEED

--- backend/src/main/java/com/project/projectIncident/services/IncidentService.java
+++ backend/src/main/java/com/project/projectIncident/services/IncidentService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.projectIncident.entities.Incident;
 import com.project.projectIncident.repositories.IncidentRepository;
@@ -14,6 +15,7 @@ public class IncidentService {
 	@Autowired
 	private IncidentRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Incident> findAll(){
 		return repository.findAll();
 	}
